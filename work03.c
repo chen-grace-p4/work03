2937181560640cf9f937c2d849046fe077198413
Grace Chen <gchen20@stuy.edu>
2021-09-22 20:38:52 -0400

    Completed work

---
+++ work03.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int problemOne(){
+   int sum = 0;
+   int i;
+   for (i = 0; i < 1000; i++){
+      if(i%3==0 || i%5==0) sum += i;
+   }
+   return sum;
+}
+
+int problemSix(){
+   int sum = 0;
+   int sumOfSq = 0;
+   int i;
+   for(i = 1; i <= 100; i++){
+      sum += i;
+      sumOfSq += i*i;
+   }
+   int sqOfSum = sum*sum;
+   return sqOfSum - sumOfSq;
+}
+
+int main(){
+   printf("problem 1 answer: %d\n", problemOne());
+   printf("problem 6 answer: %d\n", problemSix());
+}
